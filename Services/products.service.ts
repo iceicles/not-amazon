fe5108ad002262159827a45cca9f261ffd3a59f0
oshiogifted <oshiogifted2011@gmail.com>
2025-02-26 17:24:48 -0500

    wrapping calls in try/catch

--- Services/products.service.ts
+++ Services/products.service.ts
@@ -1,23 +1,31 @@
 import { Product, ProductItems } from "@/types/product.type";
 
 export const fetchAllProducts = async (page: number): Promise<ProductItems[]> => {
-  const res = await fetch(`http://localhost:4000/api/v1/products?page=${page}`);
-  if (!res.ok) {
-    throw new Error('Error fetching products...')
+  try {
+    const res = await fetch(`http://localhost:4000/api/v1/products?page=${page}`);
+    if (!res.ok) {
+      throw new Error('Error fetching products...')
+    }
+    const data = await res.json() as Product;
+    const { products } = data
+    return products;
+  } catch (err: any) {
+    console.log(err.message)
+    return []
   }
-
-  const data = await res.json() as Product;
-  const { products } = data
-  return products;
 };
 
 export const fetchProductById = async(id: string) => {
-  const res = await fetch(`http://localhost:4000/api/v1/products/${id}`)
-  if (!res.ok) {
-    throw new Error(`Error fetching product with id: ${id}`)
+  try {
+    const res = await fetch(`http://localhost:4000/api/v1/products/${id}`)
+    if (!res.ok) {
+      throw new Error(`Error fetching product with id: ${id}`)
+    }
+    const data = await res.json() as Product;
+    const { product } = data;
+    return product;
+  } catch (err: any) {
+    console.log(err.message)
+    return []
   }
-
-  const data = await res.json() as Product;
-  const { product } = data;
-  return product;
 }
--- Services/wishlist.service.ts
+++ Services/wishlist.service.ts
@@ -1,41 +1,58 @@
-import { ProductItems } from "@/types/product.type";
-import { wishlistPayload } from "@/types/wishlist.type";
+import { ProductItems } from '@/types/product.type';
+import { wishlistPayload } from '@/types/wishlist.type';
 
-
-export const createWishlistProduct = async(payload: wishlistPayload) => {
-  const res = await fetch(`http://localhost:4000/api/v1/products/wishlist`, {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      payload
-    })
-  })
-  if (!res.ok) {
-    throw new Error(`Error creating wishlist with body payload - ${payload}`)
+export const createWishlistProduct = async (payload: wishlistPayload) => {
+  try {
+    const res = await fetch(`http://localhost:4000/api/v1/products/wishlist`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        payload,
+      }),
+    });
+    if (!res.ok) {
+      throw new Error(`Error creating wishlist`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err: any) {
+    console.log(err.message);
   }
+};
 
-  const data = await res.json();
-  // const { product } = data;
-  console.log('data for wishlist - ', data)
-  return data;
-}
-
-export const deleteWishlistProduct = async(productId: string) => {
-  const res = await fetch(`http://localhost:4000/api/v1/products/wishlist/${productId}`, {
-    method: 'DELETE',
-  })
-
-  const data = await res.json()
-  return data;
-}
+export const deleteWishlistProduct = async (productId: string) => {
+  try {
+    const res = await fetch(
+      `http://localhost:4000/api/v1/products/wishlist/${productId}`,
+      {
+        method: 'DELETE',
+      }
+    );
+    if (!res.ok) {
+      throw new Error(`Error deleting wishlist`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err: any) {
+    console.log(err.message)
+  }
+};
 
 export const fetchWishlistProducts = async (wishlistGuid: string) => {
-  const queryParams = new URLSearchParams({wishlistGuid})
-  const res = await fetch(`http://localhost:4000/api/v1/products/wishlist?${queryParams}`)
-
-  const data = await res.json()
-  return data
-}
+  try {
+    const queryParams = new URLSearchParams({ wishlistGuid });
+    const res = await fetch(
+      `http://localhost:4000/api/v1/products/wishlist?${queryParams}`
+    );
+    if (!res.ok) {
+      throw new Error(`Error fetching wishlist`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (err: any) {
+    console.log(err.message)
+  }
+};
